499afc5e741917deb54fd5bba37049b2a235a610
isaacomo <aas.isaac26@gmail.co>
2022-07-06 12:40:53 -0700

    0x02-fu

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,20 @@
-#include<main.h>
+#include "main.h"
+
+/**
+ * print_tables -> prints 9 times table, starting with 0
+ * @n argument passed
+ */
+int print_table(int n)
+{
+	int i, j;
+
+	putchar('\n multiplication table of 8 and 9 are: \n');
+	{
+	for  (j =1; j <= 10; j++)
+	{
+		putchar('%d * %d = %d\n',i,j,1*j);
+	}
+	putchar('\n ==========\n');
+	}
+	return 0;
+}
