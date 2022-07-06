2a4f31aa9a1f6499972bec1525460f4c36629971
isaacomo <aas.isaac26@gmail.co>
2022-07-05 19:18:54 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#inclue "main.h"
+
+/**
+ *main - print putcar
+ * Return: always return 0
+ */
+int main(void)
+{
+	-putchar('_');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+	-putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Thi is a readme file containg 0x02. C - Functions, nested loops tasks.
