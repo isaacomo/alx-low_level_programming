fd1a5f8517531de355f7f23c15abcaa91d800bc8
isaacomo <aas.isaac26@gmail.co>
2022-07-07 11:20:25 -0700

    0x03-de

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+1#include "main.h"
+2
+3/**
+4* main - test function that prints if integer is positive or negative
+5* Return: 0
+6*/
+7
+8
+9{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
