cf9a8568b5dd7099c893f5ed5773f355956e7f9a
isaacomo <aas.isaac26@gmail.co>
2022-07-12 09:41:23 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "mani.h"
+#include <string>
+/**
+ * _strlen -> function to get the length of a string
+ * @s: string pointer to passed to this function
+ * Return: return length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
