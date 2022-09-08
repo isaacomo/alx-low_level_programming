d35ec9fd699d2a78a75e81d1f57867289d1eaec7
omorowange isaac <aas.isaac26@gmail.com>
2022-09-08 01:28:00 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and thatpiece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
