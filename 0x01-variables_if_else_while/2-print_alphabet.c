19c62b5314f758be8d9b157f280e59b854692d7f
omorowange isaac <aas.isaac26@gmail.com>
2022-09-10 02:52:24 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 /**
  * main - prints the alphabet in lowercase.
  *
- * Return: 0 on success
+ * Return: 0 on succesis
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 	while (c <= 'z')
 	{
 		putchar(c);
-		C++;
+		c++;
 	}
 	putchar('\n');
 	return (0);
