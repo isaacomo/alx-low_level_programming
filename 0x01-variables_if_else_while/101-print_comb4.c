b59174c78252c433721d9729fcb6f153558ed8a0
omorowange isaac <aas.isaac26@gmail.com>
2022-09-10 04:10:52 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,9 +6,8 @@
  * Numbers must be separated by ,followed by a space
  * The two digits must be different
  * 012, 120, 102, 021, 201, 210 are considered the same combination
- * Return: 0 on success
+ * Return: 0
  */
-
 int main(void)
 {
 	int n1 = 48;
