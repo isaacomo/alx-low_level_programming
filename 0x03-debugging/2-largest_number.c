4d3de51646d16db2ee5b917979641e34d7125a4e
omorowange isaac <aas.isaac26@gmail.com>
2022-09-15 01:27:52 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,21 +8,22 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
 	if (a >= b && a > c)
 	{
-		printf("%d is the largest number\n", 'a');
+		largest =a;
 	}
 	else if (b >= a && b > c)
 	{
-		printf("%d is the largest number\n", 'b');
+		largest b;
 	}
 	else
 	{
-		printf("%d is the largest number\n", 'c');
+		largest = c;
 	}
 
 	return (largest);
