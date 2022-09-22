5ac2d6a846ed040c53842bfc0372f2bb43e893a3
omorowange isaac <aas.isaac26@gmail.com>
2022-09-22 04:56:21 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,11 +2,18 @@
 
 /**
  * print_number - function that prints an integer.
- *
+ *@n: integer
  * Return: Nothing
  */
 
 void print_number(int n)
 {
+	int n = 0;
+
+	n++;
+	{
+	_putchar("%d", n);
+	}
+	_putchar(\n);
 	return (0);
 }
