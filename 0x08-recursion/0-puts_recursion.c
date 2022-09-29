d447bbe34ab02c194311bb1f18d193d3feb5522a
omorowange isaac <aas.isaac26@gmail.com>
2022-09-29 05:27:27 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,10 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if(*s != '\0')
 	{
+		_putchar(char *s)
+			_puts_recursion(s + 1);
+	}else
 		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
 }
